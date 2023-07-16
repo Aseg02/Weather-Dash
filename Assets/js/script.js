856607fafbd0fa4a3bcb14dcd70c5688b734e9cf
Anacristina Segovia <anasegovia505@gmail.com>
2023-07-16 18:23:54 -0500

    added comments to js, added classes to index.html

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,12 +1,12 @@
 var key = '64f2ee2a8261daa4d9f780f5b365f275';
 var city = "Denver"
 
-//Grabs the current time and date
+//Current time and date
 var date = moment().format('dddd, MMMM Do YYYY');
 var dateTime = moment().format('YYYY-MM-DD HH:MM:SS')
-
 var cityHist = [];
-//Will save the text value of the search and save it to an array and storage
+
+//Save the text value of the search, saves it to storage
 $('.search').on("click", function (event) {
 	event.preventDefault();
 	city = $(this).parent('.btnPar').siblings('.textVal').val().trim();
@@ -14,34 +14,31 @@ $('.search').on("click", function (event) {
 		return;
 	};
 	cityHist.push(city);
-
 	localStorage.setItem('city', JSON.stringify(cityHist));
 	fiveForecastEl.empty();
 	getHistory();
 	getWeatherToday();
 });
 
-//Will create buttons based on search history 
+//Create buttons based on search
 var contHistEl = $('.cityHist');
 function getHistory() {
 	contHistEl.empty();
 
 	for (let i = 0; i < cityHist.length; i++) {
-
 		var rowEl = $('<row>');
 		var btnEl = $('<button>').text(`${cityHist[i]}`)
-
-		rowEl.addClass('row histBtnRow');
-		btnEl.addClass('btn btn-outline-secondary histBtn');
+		rowEl.addClass('row histButtonRow');
+		btnEl.addClass('btn btn-outline-secondary histButton');
 		btnEl.attr('type', 'button');
-
 		contHistEl.prepend(rowEl);
 		rowEl.append(btnEl);
 	} if (!city) {
 		return;
 	}
-	//Allows the buttons to start a search as well
-	$('.histBtn').on("click", function (event) {
+
+	//Allows buttons to start search 
+	$('.histButton').on("click", function (event) {
 		event.preventDefault();
 		city = $(this).text();
 		fiveForecastEl.empty();
@@ -49,40 +46,48 @@ function getHistory() {
 	});
 };
 
-//Grab the main 'Today' card body.
+//Grabs 'today' card body.
 var cardTodayBody = $('.cardBodyToday')
-//Applies the weather data to the today card and then launches the five day forecast
+
+
+//Applies weather data to today card, launches five day forecast
 function getWeatherToday() {
 	var getUrlCurrent = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${key}`;
 
 	$(cardTodayBody).empty();
-
 	$.ajax({
 		url: getUrlCurrent,
 		method: 'GET',
 	}).then(function (response) {
 		$('.cardTodayCityName').text(response.name);
 		$('.cardTodayDate').text(date);
-		//Icons
-		$('.icons').attr('src', `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`);
-		// Temperature
+
+		//icons
+		$('.icons').attr('src', `https://openweathermap.org/img/wn/${response.weather[0].icon}@3x.png`);
+
+		// temp
 		var pEl = $('<p>').text(`Temperature: ${response.main.temp} °F`);
 		cardTodayBody.append(pEl);
-		//Feels Like
+
+		//feels like
 		var pElTemp = $('<p>').text(`Feels Like: ${response.main.feels_like} °F`);
 		cardTodayBody.append(pElTemp);
-		//Humidity
+
+		//humidity
 		var pElHumid = $('<p>').text(`Humidity: ${response.main.humidity} %`);
 		cardTodayBody.append(pElHumid);
-		//Wind Speed
+
+		//wind speed
 		var pElWind = $('<p>').text(`Wind Speed: ${response.wind.speed} MPH`);
 		cardTodayBody.append(pElWind);
-		//Set the lat and long from the searched city
+
+		//Set lat & long from searched city
 		var cityLon = response.coord.lon;
+
 		// console.log(cityLon);
 		var cityLat = response.coord.lat;
-		// console.log(cityLat);
 
+		// console.log(cityLat);
 		var getUrlUvi = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&exclude=hourly,daily,minutely&appid=${key}`;
 
 		$.ajax({
@@ -94,7 +99,8 @@ function getWeatherToday() {
 			var uvi = response.current.uvi;
 			pElUvi.append(uviSpan);
 			cardTodayBody.append(pElUvi);
-			//set the UV index to match an exposure chart severity based on color 
+
+			//sets UV index to match exposure chart based on color 
 			if (uvi >= 0 && uvi <= 2) {
 				uviSpan.attr('class', 'green');
 			} else if (uvi > 2 && uvi <= 5) {
@@ -122,7 +128,8 @@ function getFiveDayForecast() {
 	}).then(function (response) {
 		var fiveDayArray = response.list;
 		var myWeather = [];
-		//Made a object that would allow for easier data read
+
+		//Created an object, would allow for easier data read
 		$.each(fiveDayArray, function (index, value) {
 			testObj = {
 				date: value.dt_txt.split(' ')[0],
@@ -132,29 +139,33 @@ function getFiveDayForecast() {
 				icon: value.weather[0].icon,
 				humidity: value.main.humidity
 			}
-
 			if (value.dt_txt.split(' ')[1] === "12:00:00") {
 				myWeather.push(testObj);
 			}
 		})
-		//Inject the cards to the screen 
+
+		//Inject cards to screen 
 		for (let i = 0; i < myWeather.length; i++) {
 
+				//divElCard
 			var divElCard = $('<div>');
 			divElCard.attr('class', 'card text-white bg-primary mb-3 cardOne');
 			divElCard.attr('style', 'max-width: 200px;');
 			fiveForecastEl.append(divElCard);
 
+				//divElHeader
 			var divElHeader = $('<div>');
 			divElHeader.attr('class', 'card-header')
 			var m = moment(`${myWeather[i].date}`).format('MM-DD-YYYY');
 			divElHeader.text(m);
 			divElCard.append(divElHeader)
 
+				//divElBody
 			var divElBody = $('<div>');
 			divElBody.attr('class', 'card-body');
 			divElCard.append(divElBody);
 
+				//divElIcon
 			var divElIcon = $('<img>');
 			divElIcon.attr('class', 'icons');
 			divElIcon.attr('src', `https://openweathermap.org/img/wn/${myWeather[i].icon}@2x.png`);
@@ -173,11 +184,9 @@ function getFiveDayForecast() {
 	});
 };
 
-//Allows for the example data to load for Denver
+//Allows for example data to load for Austin
 function initLoad() {
-
 	var cityHistStore = JSON.parse(localStorage.getItem('city'));
-
 	if (cityHistStore !== null) {
 		cityHist = cityHistStore
 	}
@@ -186,7 +195,3 @@ function initLoad() {
 };
 
 initLoad();
-
-
-
-
